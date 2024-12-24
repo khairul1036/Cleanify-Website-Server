99e71b265077c32e23ef80f510a58f26cf780260
Khairul Islam <khairul15-4102@diu.edu.bd>
2024-12-25 00:15:12 +0600

    implement jwt and verify user by mail for secure data

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
-
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 const app = express();
+const cookieParser = require("cookie-parser");
+
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+  optionalSuccessStatus: 200,
+};
 
 // middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ah9aw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -20,6 +28,23 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+// verifyToken
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.TOKEN_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+  });
+  // console.log("hello token-->",token);
+
+  next();
+};
 
 async function run() {
   try {
@@ -36,6 +61,33 @@ async function run() {
     const bookingCollection = client
       .db("ServiceDB")
       .collection("booking_requests");
+
+    // generate jwt
+    app.post("/jwt", async (req, res) => {
+      const email = req.body;
+      // create token
+      const token = jwt.sign(email, process.env.TOKEN_KEY, {
+        expiresIn: "365d",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
+
+    // logout or clear token from cookie
+    app.get("/logout", async (req, res) => {
+      res
+        .clearCookie("token", {
+          maxAge: 0,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
 
     // create a service and store db
     app.post("/add-service", async (req, res) => {
@@ -73,15 +125,20 @@ async function run() {
     });
 
     // get create service by email to specific user
-    app.get("/my-service/:email", async (req, res) => {
+    app.get("/my-service/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
+      // check the valid user
+      const decodedEmail = req.user?.email;
+      if (decodedEmail !== email)
+        return res.status(401).send({ message: "unauthorized access" });
+
       const query = { "provider.email": email };
       const result = await serviceCollection.find(query).toArray();
       res.send(result);
     });
 
     // delete my created service
-    app.post("/delete-service/:id", async (req, res) => {
+    app.post("/delete-service/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await serviceCollection.deleteOne(query);
@@ -102,10 +159,14 @@ async function run() {
     });
 
     // get all  my-booked data by user email
-    app.get("/my-booking/:email", async (req, res) => {
+    app.get("/my-booking/:email", verifyToken, async (req, res) => {
       const isProvider = req.query.provider;
       const email = req.params.email;
-      // const query = { "userInfo.email": email };
+      // check the valid user
+      const decodedEmail = req.user?.email;
+      if (decodedEmail !== email)
+        return res.status(401).send({ message: "unauthorized access" });
+
       let query = {};
       if (isProvider) {
         query = { "provider.email": email };
@@ -127,8 +188,6 @@ async function run() {
       const result = await bookingCollection.updateOne(filter, updated);
       res.send(result);
     });
-
-
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
